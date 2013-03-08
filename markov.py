f2b798aa9c391e286ffa088355d43369c7c40153
Bit Shift <bitshift@bigmacintosh.net>
2013-03-08 19:12:49 +0000

    Start markers, too.

--- markov.py
+++ markov.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python
 
 
-def make_ngrams(orig_source, n=2, end_marker=None):
+def make_ngrams(orig_source, n=2, start_marker=None, end_marker=None):
     source = orig_source[:]  # Don't want to modify original if given a reference
+
+    if start_marker is not None:
+        if isinstance(source, str):
+            source = start_marker + source
+        else:
+            source.insert(0, start_marker)
 
     if end_marker is not None:
         if isinstance(source, str):

5f70f334193755bc0b89b8e9b23ce1793c72197f
Bit Shift <bitshift@bigmacintosh.net>
2013-03-08 18:48:23 +0000

    Added n-gram maker function.

--- markov.py
+++ markov.py
@@ -62,5 +62,18 @@ def get_notices(username, host, use_https=False, force_fetch=False):
     return notices
 
 
+def make_ngrams(orig_source, n=2, end_marker=None):
+    source = orig_source[:]  # Don't want to modify original if given a reference
+
+    if end_marker is not None:
+        if isinstance(source, str):
+            source += end_marker
+        else:
+            source.append(end_marker)
+
+    for i in range(0, len(source) - (n - 1)):
+        yield source[i:i+n]
+
+
 # TODO: turn the list of notices into a markov table
 # TODO: generate stuff based on the table

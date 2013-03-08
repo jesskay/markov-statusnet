4457c64600efc7c0a1ed04a4537d84e608dcafc8
Bit Shift <bitshift@bigmacintosh.net>
2013-03-08 19:13:07 +0000

    Add function for making a table from ngrams.

--- markov.py
+++ markov.py
@@ -18,3 +18,19 @@ def make_ngrams(orig_source, n=2, start_marker=None, end_marker=None):
 
     for i in range(0, len(source) - n):
         yield source[i:i+n+1]
+
+
+def make_markov_table(ngrams, preset_table={}):
+    markov_table = preset_table
+
+    for ngram in ngrams:
+        if isinstance(ngram, str):
+            if not ngram[:-1] in markov_table:
+                markov_table[ngram[:-1]] = []
+            markov_table[ngram[:-1]].append(ngram[-1])
+        else:
+            if not tuple(ngram[:-1]) in markov_table:
+                markov_table[ngram[:-1]] = []
+            markov_table[tuple(ngram[:-1])].append(ngram[-1])
+
+    return markov_table
